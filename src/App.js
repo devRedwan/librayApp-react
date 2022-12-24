f97887d0159f69a2f9152869f7c4d4ff733bbc95
Redwan Ahmed <ahmed.redwan7331@gmail.com>
2022-12-23 19:21:47 -0500

    Added a file for Bookinfo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import "./index.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Books from "./pages/Books";
+import Nav from "./components/Nav";
 import { books } from "./data";
+import "./index.css";
 import BookInfo from "./pages/BookInfo";
+import Books from "./pages/Books";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Nav />
-        <Route path="/" exact component={Home} />
-        <Route path="/books" render={() => <Books books={books} />} />
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
-        <Footer />
-      </div>
+      <Switch>
+        <div className="App">
+          <Nav />
+          <Route path="/" exact component={Home} />
+          <Route path="/books" exact render={() => <Books books={books} />} />
+          <Route path="/books/1" render={() => <BookInfo books={books} />} />
+          <Footer />
+        </div>
+      </Switch>
     </Router>
   );
 }
